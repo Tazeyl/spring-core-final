d34906695fbff62c03d12d6d8c2dd3d92f943a31
DmitriiSokolov <Tazeyl@yandex.ru>
2025-12-23 00:00:03 +0300

    fix

---
+++ src/main/java/sokolov/spring/utils/MoneyUtils.java
@@ -0,0 +1,18 @@
+package sokolov.spring.utils;
+
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+@Component
+public class MoneyUtils {
+
+    public static BigDecimal safeAmount(String amountStr) {
+        BigDecimal amount = new BigDecimal(amountStr);
+        if (amount.scale() > 4) {
+            throw new IllegalArgumentException("Scale too large");
+        }
+        return amount.setScale(2, RoundingMode.HALF_EVEN);
+    }
+}
